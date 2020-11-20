3e41b07ac93aea7b1cf345be3a0913f26dfababc
Benoit LUSSET <benoit.lusset@isen.yncrea.fr>
2020-11-20 09:25:11 +0100

    fin Ex1.1

--- TP5_1/TP5_ex1.c
+++ TP5_1/TP5_ex1.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "tab.h"
 #define TAILLE 10
+#define TAB2SIZE 100
 
 
 int initTab(int* tab, int size) {
@@ -18,7 +19,7 @@ int initTab(int* tab, int size) {
 int afficheTab(int* tab, int size, int nbElts) {
 
 	if (tab == NULL || size < 0 || size < nbElts) {
-		printf("-1");									 //affiche -1 car avec un return -1;, la console n'affiche rien.
+		return -1;
 	}
 	else{
 		for (int i = 0; i < nbElts; i++) {
@@ -28,11 +29,36 @@ int afficheTab(int* tab, int size, int nbElts) {
 
 }
 
+int remplirTab_1_a_20(int* tab, int size) {
+
+	if (tab == NULL || size < 0 ) {
+		return -1;
+	}
+	else {
+		for (int i = 0;i < 20; i++) {
+			*(tab + i) = i + 1;
+		}
+	}
+}
+
 
 int main() {
 
 	int myTab1[TAILLE];
-	initTab(myTab1, TAILLE);
-	afficheTab(myTab1, TAILLE, 12);
+	
+	int* myTab2 = NULL;
+	int tabSize = TAB2SIZE;
+	int nbElts = 0;
+	myTab2 = (int*)malloc(TAB2SIZE * sizeof(int));
 
+	if (myTab2 != NULL) { 
+		initTab(myTab2, tabSize);
+		remplirTab_1_a_20(myTab2, tabSize); 
+		for (int i = 0; i < 20; i++) {
+			printf("%d ", *(myTab2 + i));
+		}
+	}
+	else{ printf("Memoire insuffisante"); }
+
+	free(myTab2);
 }
